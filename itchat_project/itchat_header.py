bc123697434eb7f81057c9ca775be1045711c697
LaoBan-ywcm <249007415@qq.com>
2018-01-28 18:25:47 +0800

    feat:增加微信头像获取代码

---
+++ itchat_project/itchat_header.py
@@ -0,0 +1,73 @@
+from numpy import *
+import itchat
+import urllib
+import requests
+import os
+from pprint import pprint
+import time
+
+import PIL.Image as Image
+from os import listdir
+import math
+
+# 登录微信
+itchat.auto_login()
+friends = itchat.get_friends(update=True)[0:]
+
+user = friends[0]["UserName"]
+
+os.mkdir(user)
+
+num = 0
+
+# 下载微信头像
+for i in friends:
+    img = itchat.get_head_img(userName=i["UserName"])
+    fileImage = open(user + "/" + str(num) + ".png",'wb')
+    fileImage.write(img)
+    fileImage.close()
+    num += 1
+
+pics = listdir(user)
+
+numPic = 100
+
+print(f'照片数量:{numPic}')
+
+eachsize = int(math.sqrt(float(640 * 640) / numPic))
+
+print(f'每个照片的大小:{eachsize}')
+
+numline = int(640 / eachsize)
+
+print(f'每行照片数量:{numline}')
+
+toImage = Image.new('RGBA', (640, 640))
+
+
+x = 0
+y = 0
+n = 0
+
+for i in pics:
+    try:
+        #打开图片
+        img = Image.open(user + "/" + i)
+    except IOError:
+        pass
+    else:
+        #缩小图片
+        img = img.resize((eachsize, eachsize), Image.ANTIALIAS)
+        #拼接图片
+        toImage.paste(img, (x * eachsize, y * eachsize))
+        x += 1
+        if x == numline:
+            x = 0
+            y += 1
+        n += 1
+        # 判断是否够100张图片
+        if n == 100:
+            break
+
+
+toImage.save(user + ".png")
